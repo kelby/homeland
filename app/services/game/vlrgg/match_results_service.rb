ead1f8e37e9b0da6cb50e311f04ee0aab27b1eb5
Dev <dev@phoenixchain>
2021-08-09 13:00:58 +0800

    adjust event detail page css. modify match_results_service

--- app/services/game/vlrgg/match_results_service.rb
+++ app/services/game/vlrgg/match_results_service.rb
@@ -1,41 +1,46 @@
 module Game
   module Vlrgg
     class MatchResultsService
-      attr_accessor :id
+      attr_accessor :page_size
 
       def initialize(data='')
-        @id = data
+        @page_size = data
       end
 
-      def run
-        response = Typhoeus.get("#{ENV['spider_uri']}/vlrgg/valorant/matches", params: {page: page}, timeout: 7)
+      def run(begin_page=1)
+        page_size ||= begin_page
+        results = []
 
-        datas = if response.success?
-          body = response.body
-          # hell yeah
-          begin
-            JSON.parse(body)
-          rescue Exception => e
-            {"_error" => "json_parse_err"}
+        (1..page_size).to_a.each do |page|
+          response = Typhoeus.get("#{ENV['spider_uri']}/vlrgg/valorant/matches/results", params: {page: page}, timeout: 7)
+
+          datas = if response.success?
+            body = response.body
+            # hell yeah
+            begin
+              JSON.parse(body)
+            rescue Exception => e
+              {"_error" => "json_parse_err"}
+            end
+            # hell yeah
+          elsif response.timed_out?
+            # aw hell no
+            # log("got a time out")
+            {"_error" => "timed_out"}
+          elsif response.code == 0
+            # Could not get an http response, something's wrong.
+            # log(response.return_message)
+            {"_error" => "code_zero"}
+          else
+            {"_error" => "unknown"}
+            # Received a non-successful http response.
+            # log("HTTP request failed: " + response.code.to_s)
           end
-          # hell yeah
-        elsif response.timed_out?
-          # aw hell no
-          # log("got a time out")
-          {"_error" => "timed_out"}
-        elsif response.code == 0
-          # Could not get an http response, something's wrong.
-          # log(response.return_message)
-          {"_error" => "code_zero"}
-        else
-          {"_error" => "unknown"}
-          # Received a non-successful http response.
-          # log("HTTP request failed: " + response.code.to_s)
-        end
 
-        unless datas.is_a?(Hash) && datas["_error"].present?
-          datas.map do |data|
-            ::Vlrgg::MatchResultsService.new(data, page: page).run
+          unless datas.is_a?(Hash) && datas["_error"].present?
+            datas.map do |data|
+              ::Vlrgg::MatchResultsService.new(data, page: page).run
+            end
           end
         end
       end
